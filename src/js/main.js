f8938455bf8a66e5f45a741e73711295cf2eaf93
Mikhailo <mikhailo.kandyba0@gmail.com>
2022-07-31 13:57:53 +0300

    ready header and banner

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 $(".btn-menu").on('click', function(e) {
     e.preventDefault();
     $(this).toggleClass("active");
-    $(".menu-navigation-wrap").toggleClass('show-menu');
+    $(".header-content").toggleClass('show-menu');
     $("body").toggleClass('overflow-hidden');
 });
